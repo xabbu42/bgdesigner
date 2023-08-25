80c9c71dbfcaa43745060119336ffe6f4fc890c4
Nathan Gass <gass@search.ch>
2023-08-25 16:55:13 +0200

    add some basic layouting to rendered game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,18 @@
 	let name = 'jass.json';
 	let things = [];
 	const templates = [...allpathes(data)].filter((v) => v.startsWith('.'));
-	things = render(templates[0].substring(1), data, data);
+	things = render(templates[0].substring(1), data, data).flat(Infinity);
 </script>
 <h1>{name}</h1>
-{#each things as thing}
-<div>
-	{@html thing}
+<div class="game">
+	{#each things as thing}
+		<div class="thing">
+			{@html thing}
+		</div>
+	{/each}
 </div>
-{/each}
+
+<style>
+	.game {display: flex; flex-wrap: wrap}
+	.thing {padding: 0.5em}
+</style>
