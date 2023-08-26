c29cdafffa06b1e814c3fc8b7adbd5b3d640599d
Nathan Gass <gass@search.ch>
2023-08-26 22:14:57 +0200

    add navigation to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
 	import "../app.css";
+	import { page } from '$app/stores';
 </script>
 
-<slot />
+<div class="p-4">
+	<nav class="font-sans border-b-2 p-1 w-full">
+		<div>
+		<a href="/" class="text-2xl">BGDesigner</a>
+		<a href="/game/jass.json" class:active={$page.url.pathname === "/game/jass.json"}>jass</a>
+		<a href="/game/thecrew.json" class:active={$page.url.pathname === "/game/thecrew.json"}>thecrew</a>
+		</div>
+	</nav>
+	<slot />
+</div>
+<style lang="postcss">
+	nav a { @apply text-lg no-underline text-gray-900 hover:text-blue-800 p-2 ml-2 rounded-t-lg font-semibold }
+	.active { @apply bg-gray-200 }
+</style>
