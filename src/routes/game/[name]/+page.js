dd0365a0c7bae4d7fc4d1851e97342232cab0ab3
Nathan Gass <gass@search.ch>
2023-08-26 22:02:10 +0200

    add route parameter for different games

--- src/routes/game/[name]/+page.js
+++ src/routes/game/[name]/+page.js
@@ -1,7 +1,6 @@
 
 export async function load({ fetch, params }) {
-	let name = 'jass.json';
-	let response = await fetch("/games/" + name);
+	let response = await fetch("/games/" + params.name);
 	let game = await response.json();
 	return game;
 }
--- src/routes/game/[name]/+page.svelte
+++ src/routes/game/[name]/+page.svelte
@@ -1,13 +1,11 @@
 <script>
 	import "iconify-icon";
-	import { render,allpathes } from "../lib/render.ts";
+	import { render,allpathes } from "../../../lib/render.ts";
 	export let data;
-	let name = 'jass.json';
 	let things = [];
 	const templates = [...allpathes(data)].filter((v) => v.startsWith('.'));
-	things = render(templates[0].substring(1), data, data).flat(Infinity);
+	$: things = render(templates[0].substring(1), data, data).flat(Infinity);
 </script>
-<h1>{name}</h1>
 <div class="flex flex-wrap">
 	{#each things as thing}
 		<div class="p-1">
