1b37e7b8d333763e01ead57408f1bb8aa832f4dc
Nathan Gass <gass@search.ch>
2023-08-29 19:32:11 +0200

    do not hardcode list of games

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,17 @@
 <script>
 	import "../app.css";
 	import { page } from '$app/stores';
+	const obj = import.meta.glob('../../static/games/*');
+	const games = Object.keys(obj).map((v) => v.match(/([^\/]+)$/, v)[0]);
 </script>
 
 <div class="p-4">
 	<nav class="font-sans border-b-2 p-1 w-full">
 		<div>
-		<a href="/" class="text-2xl">BGDesigner</a>
-		<a href="/game/jass.json" class:active={$page.url.pathname === "/game/jass.json"}>jass</a>
-		<a href="/game/thecrew.json" class:active={$page.url.pathname === "/game/thecrew.json"}>thecrew</a>
+			<a href="/" class="text-2xl">BGDesigner</a>
+			{#each games as game}
+				<a href="/game/{game}" class:active={$page.url.pathname === "/game/{game}"}>{game}</a>
+			{/each}
 		</div>
 	</nav>
 	<slot />
