d00649435e92d69024438e02d47d6406c2076290
Nathan Gass <gass@search.ch>
2023-09-16 20:31:46 +0200

    same off by one for targetheight, remove debug output

--- src/lib/textfit.ts
+++ src/lib/textfit.ts
@@ -7,19 +7,16 @@ export function textfit(el, options = {}) {
 	let low = options.up ? size : 4;
 	let high = options.down ? size : 80;
 	let mid;
-	el.style.overflow = "hidden";
     while (low <= high) {
 		mid = (high + low) >> 1;
 		el.style.height = targetheight;
 		el.style.fontSize = mid + 'px';
-		console.log(mid + 'px', el.scrollHeight);
-		if(el.scrollWidth <= targetwidth + 1 && el.scrollHeight <= targetheight) {
+		if(el.scrollWidth <= targetwidth + 1 && el.scrollHeight <= targetheight + 1) {
 			size = mid;
 			low = mid + 1;
 		} else {
 			high = mid - 1;
 		}
     }
-	console.log("Final:", el, size + 'px');
 	el.style.fontSize = size + 'px';
 }
