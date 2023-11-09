6a68b4312b1ca8fe46b42823c3165200d249a51f
Nathan Gass <gass@search.ch>
2023-11-09 23:17:24 +0100

    use json5 file extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './static/games/**/*.json'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './static/games/**/*.{json,json5,jsonc}'],
   theme: {
     extend: {},
   },
