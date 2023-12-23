8c90649df8bd11dbc2aafb48568e14b8c5098a4a
Nathan Gass <gass@search.ch>
2023-12-23 13:30:28 +0100

    make tokens draggable

--- src/lib/Token.svelte
+++ src/lib/Token.svelte
@@ -1,21 +1,27 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onMount,tick } from 'svelte';
 	export let token;
 	let div;
-	/* onMount(() => {
-	   const resizeObserver = new ResizeObserver(entries => {
-	   if (div) {
-	   div.style.transform = "scale(" + (div.parentNode.offsetWidth / div.offsetWidth) + ")";
-	   }
-	   });
-	   resizeObserver.observe(div.parentNode);
-	   return () => resizeObserver.unobserve(div);
-	   }); */
+	let pos = {x:0, y:0};
+	onMount(async () => {
+		let rect = div.getBoundingClientRect();
+		let initialpos = {x: rect.left, y: rect.top};
+		await tick();
+		div.style = "position: absolute";
+		pos = initialpos;
+	});
 </script>
 
 <div
 	bind:this="{div}"
 	on:dblclick|preventDefault="{(e) => {token.flip(); token = token}}"
+	on:mousedown|preventDefault|stopPropagation="{(e) => {div.parentNode.appendChild(div)}}"
+	on:mousemove|preventDefault|stopPropagation="{(e) => {
+		if (e.buttons == 1) {
+			pos = {x: pos.x + e.movementX, y: pos.y + e.movementY};
+		}
+	}}"
+	style="left: {pos.x}px; top: {pos.y}px"
 >
 	{@html token}
 </div>
