561e2a7de0a8faef709d4f6e31a044440889117a
Nathan Gass <gass@search.ch>
2024-01-14 16:30:53 +0100

    simplify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 		<div>
 			<a href="/" class="text-2xl">BGDesigner</a>
 			{#each games as game}
-				<a data-sveltekit-reload href="/game/{game}" class:active={$page.url.pathname === "/game/{game}"}>{game}</a>
+				<a data-sveltekit-reload href="/game/{game}" class:active={$page.params.name === game}>{game}</a>
 			{/each}
 		</div>
 	</nav>
