6a9d610157f2f3bfd62ef1234e83aa2b6d9547c3
Nathan Gass <gass@search.ch>
2024-09-24 22:14:34 +0200

    add locations for dragin and visualize them

--- src/routes/game/[name]/[play]/+page.svelte
+++ src/routes/game/[name]/[play]/+page.svelte
@@ -22,6 +22,33 @@ let space;
 let entered = false;
 let members = {};
 let cursors = {};
+let locations = {};
+let locks = {};
+
+function handle_lock(lock) {
+	if (lock.member.connectionId != $ably.connection.id) {
+		let obj = $game.render(lock.id);
+		if (lock.status == 'unlocked') {
+			obj.usermode = UserMode.None;
+			delete locks[lock.member.connectionId];
+		} else if (lock.status == 'locked') {
+			obj.usermode = locations[lock.member.connectionId] && locations[lock.member.connectionId].path == lock.id ? UserMode.Drag : UserMode.Hover;
+			obj.usercolor = members[lock.member.connectionId].profileData.color;
+			locks[lock.member.connectionId] = obj;
+		}
+		$game = $game;
+	}
+}
+
+function handle_message(msg) {
+	if (msg.connectionId != $ably.connection.id) {
+		let obj = msg.data.path ? $game.render(msg.data.path) : $game;
+		let res = obj[msg.data.action](...(msg.data.args || []).map(p => p ? $game.render(p) : p));
+		if (msg.data.pos)
+			res.pos = msg.data.pos;
+		$game = $game;
+	}
+}
 
 ably.subscribe(async a => {
 	if (a) {
@@ -36,8 +63,11 @@ ably.subscribe(async a => {
 		space = await $spaces.get(key);
 		await space.enter($user);
 		user.subscribe(u => space.updateProfileData(u));
-		members = Object.fromEntries((await space.getState()).members.map(v => [v.connectionId, v]));
-		cursors = Object.fromEntries(Object.entries(await space.cursors.getAll()).filter(v => v[1] != undefined));
+		members   = Object.fromEntries((await space.getState()).members.map(v => [v.connectionId, v]));
+		locations = await space.locations.getOthers();
+		cursors   = Object.fromEntries(Object.entries(await space.cursors.getAll()).filter(v => v[1] != undefined));
+		(await space.locks.getOthers()).map(l => handle_lock(l));
+		space.locks.subscribe(handle_lock);
 		space.members.subscribe(['leave', 'remove'], (ms) => {
 			delete cursors[ms.connectionId];
 			delete members[ms.connectionId];
@@ -48,45 +78,29 @@ ably.subscribe(async a => {
 			members[ms.connectionId] = ms;
 			members = members;
 		});
+		space.locations.subscribe('update', (ms) => {
+			locations[ms.member.connectionId] = ms.currentLocation;
+			if (locks[ms.member.connectionId]) {
+				locks[ms.member.connectionId].usermode = locks[ms.member.connectionId].path == ms.currentLocation.path ? UserMode.Drag : UserMode.Hover;
+				$game = $game;
+			}
+		});
 		entered = true;
 		space.cursors.subscribe(async (update) => {
 			cursors[update.connectionId] = update;
 			cursors = cursors;
 		});
-		space.locks.subscribe((lock) => {
-			if (lock.member.connectionId != $ably.connection.id) {
-				let obj = $game.render(lock.id);
-				if (lock.status == 'unlocked')
-					obj.usermode = UserMode.None;
-				else if (lock.status == 'locked') {
-					obj.usermode = UserMode.Hover;
-					obj.usercolor = members[lock.member.connectionId].profileData.color;
-				}
-				$game = $game;
-			}
-		});
 	}
 });
 
-let position;
-function onpointermove({clientX, clientY}) {
-	if (entered)
-		space.cursors.set({position: {x: clientX, y: clientY}});
-}
-
 onDestroy(() => {
-	if (space)
+	if (entered)
 		space.leave();
 });
 
-function handle_message(msg) {
-	if (msg.connectionId != $ably.connection.id) {
-		let obj = msg.data.path ? $game.render(msg.data.path) : $game;
-		let res = obj[msg.data.action](...(msg.data.args || []).map(p => p ? $game.render(p) : p));
-		if (msg.data.pos)
-			res.pos = msg.data.pos;
-		$game = $game;
-	}
+function onpointermove({clientX, clientY}) {
+	if (entered)
+		space.cursors.set({position: {x: clientX, y: clientY}});
 }
 
 function ongameevent(e) {
@@ -110,6 +124,10 @@ function onuievent(e) {
 			space.locks.acquire(e.detail.path);
 			locked = e.detail.path;
 		}
+		if (e.detail.action == 'drag')
+			space.locations.set({path: e.detail.path});
+		else if (e.detail.action == 'drop')
+			space.locations.set({path: undefined});
 	}
 }
 
