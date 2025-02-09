00944e61a526a54a778f83fc3190c91953cb3ee3
Nathan Gass <gass@search.ch>
2025-02-09 14:19:21 +0100

    move client-side user name handling (random default, persistance with sessionStorage) to init_ably so its part of the library

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,16 +1,8 @@
 import type { ClientInit } from '@sveltejs/kit';
-
-import rug from 'random-username-generator';
-
-import { hashcolor } from '$lib/utils';
-import { user } from '$lib/stores';
 import { init_ably } from '$lib/init_ably';
 import * as env from '$env/static/public';
 
 export const init: ClientInit = async () => {
-	let name = sessionStorage.getItem('username') || rug.generate();
-	let u = {name, color: hashcolor(name)};
-	user.set(u);
 	if ('PUBLIC_ABLY_KEY' in env)
 		init_ably(env.PUBLIC_ABLY_KEY);
 };
--- src/lib/init_ably.ts
+++ src/lib/init_ably.ts
@@ -2,9 +2,14 @@ import Ably from 'ably';
 import { get } from 'svelte/store';
 import { user } from '$lib/stores';
 
+import rug from 'random-username-generator';
+import { hashcolor } from '$lib/utils';
+
 export let ably;
 export function init_ably(key) {
 	console.log("init ably");
-	let u = get(user);
+	let name = sessionStorage.getItem('username') || rug.generate();
+	let u = {name, color: hashcolor(name)};
+	user.set(u);
 	ably = new Ably.Realtime({key, clientId: u.name});
 }
